da3c84281afc2682f08ea5c4e985913ad925f048
Marcos Martinez <marcos.martinez.clemente@gmail.com>
2024-05-17 18:31:52 +0200

    - Added: Auth complete

--- src/main/java/com/ioc/m08_eac4/M08Eac4Application.java
+++ src/main/java/com/ioc/m08_eac4/M08Eac4Application.java
@@ -9,8 +9,7 @@ public class M08Eac4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(M08Eac4Application.class, args);
 
-		System.out.println("Marcos Martinez Clemente");
-		System.out.println("He aprés molt fent el mòdul 8 de DAW encara que hagi estat dur!!!");
+		System.out.println("User Login Started");
 	}
 
 }
